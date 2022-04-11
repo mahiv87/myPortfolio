be1a37eba05586120fc9214726c56b2b201427d0
Marcus Herrera <maherreraiv1987@gmail.com>
2022-04-11 08:37:49 -0600

    Changed content in MY WORK section. Added tailwindcss animation

--- main.js
+++ main.js
@@ -15,3 +15,14 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+const burger = document.querySelector('#burger');
+const menu = document.querySelector('#menu');
+
+burger.addEventListener('click', () => {
+    if (menu.classList.contains('hidden')) {
+        menu.classList.remove('hidden');
+    } else {
+        menu.classList.add('hidden');
+    }
+})
